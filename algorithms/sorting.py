30ccf2f5ccf08cdc7ac05d03929bf78c91decf33
Brooklyn Bowers <brooklynbowers0914@example.com>
2025-01-11 11:08:40 -0500

    completed the bubble sort algorithm code

--- algorithms/sorting.py
+++ algorithms/sorting.py
@@ -1,1 +1,14 @@
 # Bubble Sort Algorithm
+
+def bubble_sort(arr):
+    for i in range(len(arr) - 1):
+        # i represents each value in the array, the range 
+        # is the 1 minus the length of the array 
+        for j in range(len(arr)- i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+        # loop through each index of the array, swap if the first index value is 
+        # larger than the second index value
+    return arr
+
+print(bubble_sort([4, 3, 5, 6]))
